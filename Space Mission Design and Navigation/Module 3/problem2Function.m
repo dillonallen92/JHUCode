70ef4ffc334fc6eee15f54c894c9396958e54729
Dillon Allen <dillona@cos.edu>
2021-02-16 13:44:00 -0800

    Updated Function2
    Still have a pesky negative that I cannot resolve, will do soon

--- Space Mission Design and Navigation/Module 3/problem2Function.m
+++ Space Mission Design and Navigation/Module 3/problem2Function.m
@@ -5,13 +5,34 @@ function [r,v] = problem2Function(a,e,i,Omega,omega,theta)
 
     % Constants
     mu = 132712440041.94; % km^3/s^2
-    auToKm = 149597870.7; % km
-    dayToSeconds = 1/(24*3600);
-
+    KmInAU = 149597870.7; % AU/ km
+    secondsInDay = 24*3600; % days / second
+    
     % Using Prussing as a guide
-
-    theta_epoch = omega + theta;
-    r_norm = (a*(1-e^2))/(1+e*cos(theta_epoch));
-
+    a = a * KmInAU;
+    r_norm = (a*(1-e^2))/(1+e*cos(theta));
+    theta = theta + omega; % theta in prussing is theta + omega for us
+    
+    r_x = r_norm*(cos(Omega)*cos(theta) - sin(Omega)*sin(theta)*cos(i));
+    r_y = r_norm*(sin(Omega)*cos(theta) + cos(Omega)*sin(theta)*cos(i));
+    r_z = r_norm*sin(theta)*sin(i);
+    
+    r = [r_x; r_y; r_z];
+    r = r / KmInAU;
+    
+    % Velocity
+    h = sqrt(mu*a*(1-e^2));
+    sinTerms = sin(theta) + e*sin(omega);
+    cosTerms = cos(theta) + e*cos(omega);
+    muh = mu / h;
+    
+    v_x = -muh * (cos(Omega) * sinTerms + sin(Omega)*cosTerms*cos(i));
+    v_y = muh * (sin(Omega)*sinTerms - cos(Omega)*cosTerms*cos(i));
+    v_z = muh * cosTerms * sin(i);
+    
+    v = [v_x; v_y; v_z];
+    
+    v = v * secondsInDay / KmInAU;
+    
     
 end
---
+++ Space Mission Design and Navigation/Module 3/problem2_starman.m
@@ -0,0 +1,13 @@
+clear, clc, close all;
+
+% This script is to test function 2 to recreate the starman position and
+% velocity vectors
+
+a = 1.32489106722386;
+e = 0.255915584525353;
+omega = 3.09848024449946;
+i = 0.0188055315710807; 
+Omega = 5.53414135342721;
+theta = 3.88465273236322;
+
+[r,v] = problem2Function(a,e,i, Omega, omega, theta)
