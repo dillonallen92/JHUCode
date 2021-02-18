ff34947345e09d0ac646af2531b33679de6975d7
Dillon Allen <dillona@cos.edu>
2021-02-18 13:20:30 -0800

    Create problem4_and_5.m

---
+++ Space Mission Design and Navigation/Module 3/problem4_and_5.m
@@ -0,0 +1,15 @@
+clear, clc, close all;
+
+%% Problem 4
+% Calculate the amount of time that starman spends its orbit under 1 AU.
+% Note that you must make sure that the second mean anomaly is greater than
+% the first by adding 2*pi
+
+
+
+
+%% Problem 5
+% Independently calculate the amount of time that starman spends above 1
+% AU and verify that this and the value obtained in 4 add up to the orbital
+% period.
+
