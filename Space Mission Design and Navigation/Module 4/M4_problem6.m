6583087883a573e17959564c6882a813b4814ed7
Dillon Allen <dillona@cos.edu>
2021-02-28 15:07:15 -0800

    Made code prettier?

--- Space Mission Design and Navigation/Module 4/M4_problem6.m
+++ Space Mission Design and Navigation/Module 4/M4_problem6.m
@@ -23,29 +23,33 @@ c = [p0 p1 p2 p3 p4]';
 xvec0 = [x0 y0 xdot0 ydot0 g]';
 tol = 1E-5;
 
-while 1
-   J = c - G(xvec0, xs, ys);
-   xp = xvec0 + pJ(xvec0, xs, ys)\J; % My pJ is - pG/pX0 so there is a +
-   if norm(xp - xvec0) < tol
-       disp('Real initial values:');
-       xp
-       break;
-   end
-   xvec0 = xp;
-end
+xp = NewtonScheme(c, xvec0, xs, ys, tol);
 
+disp("-- Actual Initial Conditions --")
+fprintf("x0: %d\n", xp(1));
+fprintf("y0: %d\n", xp(2));
+fprintf("xdot0: %d\n", xp(3));
+fprintf("ydot0: %d\n", xp(4));
+fprintf("g0: %d\n", xp(5));
+
+% Functions
+
+% X component helper function
 function val = xcomp(xvec, t, xs)
     val = xvec(1) - xs + xvec(3)*t;
 end
 
+% Y component helper function
 function val = ycomp(xvec,t,ys)
     val = xvec(2) - ys + xvec(4)*t - .5*xvec(5)*t^2;
 end
 
+% Range Equation
 function val = rho(xvec, t, xs, ys)
     val = sqrt(xcomp(xvec,t, xs)^2 + ycomp(xvec, t,ys)^2);
 end
 
+% Initial G vector function
 function val = G(xvec,xs,ys)
     val = zeros(5,1);
     for i = 1 : 5
@@ -54,6 +58,7 @@ function val = G(xvec,xs,ys)
     end
 end
 
+% Matrix of partial derivatives
 function val = pJ(xvec, xs, ys)
     val = zeros(5); % 5 x 5 matrix
     for i = 1 : 5
@@ -63,5 +68,17 @@ function val = pJ(xvec, xs, ys)
        val(i,3) = (t * xcomp(xvec, t, xs)) / rho(xvec, t, xs, ys);
        val(i,4) = (t * ycomp(xvec, t, ys)) / rho(xvec, t, xs, ys);
        val(i,5) = (-t^2*ycomp(xvec, t, ys))/(2*rho(xvec, t, xs, ys));
+    end
+end
+
+% Newtons Method Function
+function xp = NewtonScheme(c, xvec, xs, ys, tol)
+    while 1
+       J = c - G(xvec, xs, ys);
+       xp = xvec + pJ(xvec, xs, ys)\J; % My pJ is - pG/pX0 so there is a +
+       if norm(xp - xvec) < tol
+           break;
+       end
+       xvec = xp;
     end
 end
