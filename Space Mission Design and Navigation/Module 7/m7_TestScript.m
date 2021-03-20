7d97e7b9abd64235780c02d91f874fb01aa8b429
Dillon Allen <dillona@cos.edu>
2021-03-19 21:21:59 -0700

    Added sections for testing each problems function

---
+++ Space Mission Design and Navigation/Module 7/m7_TestScript.m
@@ -0,0 +1,16 @@
+clear, clc, close all;
+
+% This script is the test script that will be used to run all the functions
+% needed in the homework with the data provided from Blackboard. 
+
+%% Problem 1 (Nav_Sol)
+
+%% Problem 2 (Ideal Range)
+
+%% Problem 3 (Ideal Range Rate)
+
+%% Problem 4 (Integrated Carrier Phase)
+
+%% Problem 5 (Azimuth / Elevation)
+
+%% Problem 6 (TDOA / FDOA)
