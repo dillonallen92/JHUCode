ebcf06a3557acd3b3464ed465c19082914cb8626
Dillon Allen <dillona@cos.edu>
2021-04-12 16:18:09 -0700

    Update m9_prob4.m

--- Space Mission Design and Navigation/Module 9/m9_prob4.m
+++ Space Mission Design and Navigation/Module 9/m9_prob4.m
@@ -3,7 +3,7 @@ clear, clc, close all;
 % Use the following state vector 
 xvec = [7378.05e3; -33.75; -14.667; 0.03; 7.35e3; 0];
 
-%% Part A
+%% Part B
 % for zero control and disturbance, prop for 105 min.
 tspan = 0:0.1:(105*60);
 u = [ 0 0 0]';
