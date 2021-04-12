9c790c2c31a252639eaed35a8986c91a08860874
Dillon Allen <dillona@cos.edu>
2021-04-12 15:15:38 -0700

    part E and F mainly complete

--- Space Mission Design and Navigation/Module 9/m9_prob2.m
+++ Space Mission Design and Navigation/Module 9/m9_prob2.m
@@ -42,9 +42,27 @@ xComb0 = [r(1); r(2); v(1); v(2); reshape(eye(4),16,1)];
 [t,x] = ode113(@(t,x)twoBody(t,x,muE),tspan,xComb0, options);
 
 xfinal = x(end,1:20);
-STMFinal = reshape(x(end,5:20),4,4)';
 
+%% Part E
+% Reform the last state back into the 4x4 STM
+% Identify the matrix element which predicts the change in the final
+% x-component of position with the initial y-component of velocity. Use
+% this element to predict the change in the final x-component of position
+% for a 0.001 km/s increase in Vy.
 
+STMFinal = reshape(xfinal(1,5:20),4,4)';
+
+%% Part F
+% Propogate with part d
+rnew = [42241.0800678832 0]';
+vnew = [0 3.07285802826297]';
+xCombNew = [rnew(1); rnew(2); vnew(1); vnew(2); reshape(eye(4),16,1)];
+[tnew, xnew] = ode113(@(t,x)twoBody(t,x,muE),tspan,xCombNew,options);
+
+xnew_final = xnew(end, 1:20);
+xnew_final_x = xnew(1,1);
+xfinal_x = xfinal(1,1);
+partF_difference = xnew_final_x - xfinal_x;
 
 
 %% Functions
