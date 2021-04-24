a74696b690274c7fe7a81d96731d14525496e885
Dillon Allen <dillona@cos.edu>
2021-04-24 12:25:29 -0700

    Update azel_example.m

--- Space Mission Design and Navigation/Module 11/examples/azel_example.m
+++ Space Mission Design and Navigation/Module 11/examples/azel_example.m
@@ -1,7 +1,7 @@
 clear, clc, close all;
 
 % Attempt on the Az/El example as seen on slides 37 - 41
-load('NLWLS_Example.mat');
+load('NLWLS_Ex.mat');
 
 % Process
 % Convert from ECEF to ENV Coordinates
@@ -14,5 +14,74 @@ load('NLWLS_Example.mat');
 
 % Questions:
 % where do I get the measurements for the example problem
+%   - NLWLS_Ex has the zVec measurement vector
 % How do I do this problem with 3 instrument locations
+%   - run the measurement model 3 times, make a full [h] matrix and [H]
+%     matrix
 % The flow chart in 4.6 uses the STM, but the notes have a process without it, which one?
+%   - no need for STM because all at single time epoch
+
+r1_IVec = rStation1_m;
+r2_IVec = rStation2_m;
+r3_IVec = rStation3_m;
+rVec = rSat_ECEF_m;
+xVec1 = rSat_ECEF_m - r1_IVec;
+xVec2 = rSat_ECEF_m - r2_IVec;
+xVec3 = rSat_ECEF_m - r3_IVec;
+x_star = x_0;
+t = 0;
+opts.t = 0;
+[h1,H1] = AzElMeasurementModel(xVec1,t,opts);
+[h2,H2] = AzElMeasurementModel(xVec2,t,opts);
+[h3,H3] = AzElMeasurementModel(xVec3,t,opts);
+h = [h1; h2; h3];
+H = [H1; H2; H3];
+
+% First Calculation before loop
+
+function [z,H] = AzElMeasurementModel(x,t,opts)
+
+    % Constant
+    omegaEarth = 7.2921159e-5;
+    deltaT = t - opts.t;
+    
+    % Measurement equations here:
+    % zOrh = ???
+    
+    x = [x(1) x(2) x(3)]';
+    rtVec = RotTrop(x) * (RotZ(omegaEarth, deltaT) * x);
+    rt = sqrt(rtVec(1)^2 + rtVec(2)^2 + rtVec(3)^2);
+    z = [atan2(rtVec(2), rtVec(1)); asin(rtVec(3)/rt)];
+    
+    % Numerical Jacobian
+    epsilon = 1e-7;
+    for j = 1:2
+        for i = 1:3
+        x(i) = x(i) + epsilon;
+        rtVecptrb = RotTrop(x) * (RotZ(omegaEarth, deltaT) * x);
+        rtptrb = sqrt(rtVecptrb(1)^2 + rtVecptrb(2)^2 + rtVecptrb(3)^2);
+        h(:,i) = [atan2(rtVecptrb(2), rtVecptrb(1)); asin(rtVecptrb(3)/rtptrb)];
+        H(:,i) = (h(i) - z)/epsilon;
+        x(i) = x(i) - epsilon;
+        end
+    end
+    
+    % Helper functions for transforming frames
+    function zRotationMat = RotZ(omegaEarth, deltaT)
+        theta = omegaEarth * deltaT;
+        zRotationMat = [cos(theta), sin(theta), 0;
+        -sin(theta), cos(theta), 0;
+        0, 0, 1;];
+    end
+    
+    function tropRotMat = RotTrop(x)
+        xy = sqrt(x(1)^2 + x(2)^2);
+        lambda = acos(x(1)/xy);
+        phi = atan2(x(3), xy);
+        tropRotMat = [-sin(lambda), cos(lambda), 0;
+        -sin(phi)*cos(lambda), -sin(phi)*sin(lambda), cos(phi);
+        cos(phi)*cos(lambda), cos(phi)*sin(lambda), sin(phi)];
+    end
+
+end
+
