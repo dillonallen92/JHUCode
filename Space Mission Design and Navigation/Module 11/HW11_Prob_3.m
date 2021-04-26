15cb74b0b6f0f5a2905be46f060257ee81e6749a
Dillon Allen <dillona@cos.edu>
2021-04-25 23:33:04 -0700

    added error per iteration plot

--- Space Mission Design and Navigation/Module 11/HW11_Prob_3.m
+++ Space Mission Design and Navigation/Module 11/HW11_Prob_3.m
@@ -11,9 +11,12 @@ xVec = zVec;
 t = 0;
 w = 0;
 
+% Truncate xVecTrue to remove z,vx,vy,vz dependance
+xVecTrue_trunc = [xVecTrue(1); xVecTrue(2)];
+
 % Initiate x_star
 x_star = x0;
-
+j = 1;
 % Loop til convergence
 while 1
     [h1, H1, ~] = Provided_measFxnRange(x_star, w, t, opts1);
@@ -21,14 +24,26 @@ while 1
     h = [h1; h2];
     H = [H1; H2];
     x_hat = x_star - (H'*H)*H'*(h - zVec);
-    if (norm(x_hat - x_star) < 1e-6)
+    if (norm(x_hat - x_star) < 1e-8)
         disp('converge');
         xVec_converge = x_hat;
+        xVecHist(:,j) = xVec_converge;
+        errHist(j) = norm(x_hat - xVecTrue_trunc);
         break;
     else
+        xVecHist(:,j) = x_hat;
+        errHist(j) = norm(x_hat - xVecTrue_trunc);
         x_star = x_hat
+        j = j + 1;
     end
 end
+
+% Plot of error vs iteration count
+itVec = 1:1:length(errHist);
+plot(errHist, itVec);
+xlabel("Iteration #");
+ylabel("Estimated State Error");
+title("Estimated Error vs Iteration Count");
 
 % Function to test truncating
 function [h,HMat,id] = Provided_measFxnRange(xVec,w,t,opts)
