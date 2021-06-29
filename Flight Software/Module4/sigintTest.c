c6cd7b30d0770d0d04666ba4bb96dbea85cd19b7
dillonallen92 <dillona@cos.edu>
2021-06-29 09:36:45 -0700

    added kill command for double press

--- Flight Software/Module4/sigintTest.c
+++ Flight Software/Module4/sigintTest.c
@@ -2,21 +2,38 @@
 #include <unistd.h>
 #include <signal.h>
 
+// Global increment value, not sure how to handle this otherwise.
+int incr = 1;
+
+// Signal Handler to take any signal
 void int_hdl(int signo)
 {
     if(signo == SIGINT)
     {
-        printf("pressed the SIGINT thing\n");
+        if(incr < 4)
+        {
+            incr = incr + 1;
+        }
+        printf("SIGINT Received! Increment is now %d\n", incr);
     }
     
     if(signo == SIGTERM)
     {
-        printf("Pressed the SIGTERM thing\n");
+        if(incr > -4)
+        {
+            incr = incr - 1;
+        }
+        printf("SIGTERM Received! Increment is now %d\n", incr);
     }
 }
 
 int main()
 {
+    int val = 0;
+    int val1 = 0;
+    int diff = 0;
+
+    // Compare the particular signals with the handler. If not found, exit.
     if(signal(SIGINT, int_hdl) == SIG_ERR)
     {
         return 1;
@@ -29,8 +46,15 @@ int main()
 
     while(1)
     {
-        printf("Hello world\n");
+        val1 = val;
+        printf("%d\n", val);
         sleep(1);
+        val = val + incr;
+        diff = val - val1;
+        if (diff >= 2){
+            printf("SIGINT Pressed Twice. Exiting Now\n");
+            return 1;
+        }
     }
     return 0;
 }
