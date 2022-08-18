9e475c1a4804edde6566d5e800ef9e568c88fd7d
dillonallen92 <dillon.allen.92@gmail.com>
2022-08-18 00:01:05 -0700

    Update problem1_code.R
    finished all the required tests, now just need to plot and make inferences

--- Mathematical Stats/project/problem1_code.R
+++ Mathematical Stats/project/problem1_code.R
@@ -22,19 +22,19 @@ fish_ph7 <- fish_split$`7`
 
 # tests for pH 5.5 
 t.test(fish_ph55$Guarding, fish_ph55$Fanning, paired = TRUE) # t = 9.8986, alt hyp
-binom.test(length((fish_ph55$Guarding - fish_ph55$Fanning) > 0), length(fish_ph55$Guarding)) #p-val = 1.9e-6
+binom.test(sum((fish_ph55$Guarding - fish_ph55$Fanning) > 0), length(fish_ph55$Guarding)) #p-val = 4.005e-5
 
 # tests for ph6
 t.test(fish_ph6$Guarding, fish_ph6$Fanning, paired = TRUE) # t = 4.1506, alt hyp
-binom.test(length((fish_ph6$Guarding - fish_ph6$Fanning) > 0), length(fish_ph6$Guarding)) #p-val = 1.9e-6
+binom.test(sum((fish_ph6$Guarding - fish_ph6$Fanning) > 0), length(fish_ph6$Guarding)) #p-val = 0.04139
 
 # test for ph6.5
 t.test(fish_ph65$Guarding, fish_ph65$Fanning, paired = TRUE) # t = 5.3816, alt hyp
-binom.test(length((fish_ph65$Guarding - fish_ph65$Fanning) > 0), length(fish_ph65$Guarding)) #p-val = 1.9e-6
+binom.test(sum((fish_ph65$Guarding - fish_ph65$Fanning) > 0), length(fish_ph65$Guarding)) #p-val = .0004025
 
 # test for ph7
 t.test(fish_ph7$Guarding, fish_ph7$Fanning, paired = TRUE) # t = 5.3816, alt hyp
-binom.test(length((fish_ph7$Guarding - fish_ph7$Fanning) > 0), length(fish_ph7$Guarding)) #p-val = 1.9e-6
+binom.test(sum((fish_ph7$Guarding - fish_ph7$Fanning) > 0), length(fish_ph7$Guarding)) #p-val = .8238
 
 # Split by genders
 fish_gender_split <- split(fish, fish$Gender)
@@ -42,9 +42,12 @@ fish_male <- fish_gender_split$M
 fish_female <- fish_gender_split$F
 
 # male test
-
+t.test(fish_male$Guarding, fish_male$Fanning, paired = TRUE) # p-val = 2.2e-16
+binom.test(sum((fish_male$Guarding - fish_male$Fanning)>0), length(fish_male$Guarding)) #p-val = 1.8e-12
 
 # female test
+t.test(fish_female$Guarding, fish_female$Fanning, paired=TRUE) #p-val = 0.1455
+binom.test(sum((fish_female$Guarding - fish_female$Fanning)>0), length(fish_female$Guarding)) #p-val = 0.4296
 
 # now it is time to start plotting things
 library(ggplot2)
